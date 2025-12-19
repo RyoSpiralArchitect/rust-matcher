e912272067782f3309fb9476cb4b4ec133ec0ad3
Ryo ∴ SpiralArchitect <kishkavsesvit@icloud.com>
2025-12-18 22:32:26 -0800

    Add location and remote corrections per README

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub mod corrections;
+pub mod matching;
+pub mod queue;
+pub mod schema;
+
+// Commonly used data models for matching functions.
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct Project {
+    pub work_todofuken: Option<String>,
+    pub work_area: Option<String>,
+    pub remote_onsite: Option<String>,
+}
+
+#[derive(Debug, Clone, Default, PartialEq)]
+pub struct Talent {
+    pub residential_todofuken: Option<String>,
+    pub residential_area: Option<String>,
+}
